1dee069771972f44c9da7821d06f687a973850fc
Guilherme Gomes <guilhermeneto26@hotmail.com>
2025-04-26 19:37:39 -0300

    Implementa comparador por ID

--- src/appPet/model/IDComparador.java
+++ src/appPet/model/IDComparador.java
@@ -1,4 +1,13 @@
 package appPet.model;
 
-public class IDComparador {
+import java.util.Comparator;
+
+
+public class IdComparador implements Comparator<Pet> {
+
+    @Override
+    public int compare(Pet p1, Pet p2) {
+        return Integer.compare(p1.getId(), p2.getId());
+    }
+
 }
