062d4c085370333ebb1cc389454d7cfc78570f51
Manuely Meireles Lemos <manumeireleslemos@gmail.com>
2025-05-08 18:27:05 -0300

    Mudança estética do menu

--- src/appPet/view/Menu.java
+++ src/appPet/view/Menu.java
@@ -7,15 +7,19 @@ public class Menu {
     public static int exibirMenu() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\n--- Menu Pet Adocao ---");
-        System.out.println("1. Adicionar Pet");
-        System.out.println("2. Pesquisar Pet por ID");
-        System.out.println("3. Pesquisar Pet por Nome");
-        System.out.println("4. Remover Pet por ID");
-        System.out.println("5. Listar Pets em Ordem (por ID)");
-        System.out.println("6. Listar Pets por Nivel (em Largura)");
-        System.out.println("0. Sair");
-        System.out.print("Escolha uma opção: ");
+        System.out.println("\n+----------------------------------+");
+        System.out.println("|         MENU PET ADOÇÃO          |");
+        System.out.println("+----------------------------------+");
+        System.out.println("| 1 | Adicionar Pet                |");
+        System.out.println("| 2 | Pesquisar Pet por ID         |");
+        System.out.println("| 3 | Pesquisar Pet por Nome       |");
+        System.out.println("| 4 | Remover Pet por ID           |");
+        System.out.println("| 5 | Listar Pets por ID (Ordem)   |");
+        System.out.println("| 6 | Listar Pets por Nível        |");
+        System.out.println("| 0 | Sair                         |");
+        System.out.println("+----------------------------------+");
+        System.out.print("Digite sua opção: ");
+
         return Integer.parseInt(scanner.nextLine());
     }
 }
